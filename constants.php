81093509def7e50c1aa77006d7eb548985923324
Michael Cervantes <mac95860@hotmail.com>
2020-11-29 15:16:59 -0600

    ways to declare constants

---
+++ constants.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+        $number = 10;
+        echo $number . "<br>";
+
+        define("NAME", 1000);
+        echo NAME . "<br>";
+
+        const APPLE = "apple";
+        echo APPLE;
+    ?>
+    
+</body>
+</html>
